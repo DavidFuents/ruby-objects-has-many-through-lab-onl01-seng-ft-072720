be5e31c831fa698e662a548d8f7b52bd27098b62
David Fuentes <davidf0827@gmail.com>
2020-08-15 01:31:58 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,4 +12,18 @@ class Patient
   def self.all
     @@all
   end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
 end
